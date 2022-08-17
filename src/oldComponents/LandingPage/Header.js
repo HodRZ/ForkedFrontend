6f8b0e515e5b334b7c204b8c63c74bac5166611a
HodRZ <hodaifa.radwan@gmail.com>
2022-08-17 03:46:24 +0300

    Add main component navbar

--- src/oldComponents/LandingPage/Header.js
+++ src/oldComponents/LandingPage/Header.js
@@ -15,27 +15,21 @@ import React from 'react';
 import Navbar from './Navbar'
 
 export default function Header() {
-    // const [showBasic, setShowBasic] = useState(false);
 
     return (
         <>
             <header>
                 <div
                     className='p-5 text-center bg-image'
-                    style={{ backgroundImage: "url('https://universes.art/fileadmin/_migrated/gridelement_uploads/00-1-IMG_0972-2000-750-2_01.jpg')", height: '25rem' }}
+                    style={{ backgroundImage: "url('https://universes.art/fileadmin/_migrated/gridelement_uploads/00-1-IMG_0972-2000-750-2_01.jpg')", height: '75rem' }}
                 >
                     <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
                         <Navbar />
-                        <div className='d-flex justify-content-center align-items-center h-100'>
-                            <div className='text-white'>
-                                <h1 className='mb-3'>YallaJo</h1>
-                                {/* <h4 className='mb-3'></h4> */}
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </header>
-            {/* <Outlet /> */}
+
         </>
     );
 }
